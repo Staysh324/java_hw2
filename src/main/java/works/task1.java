426e8b938bebb374100ba2914495e7476aa7dceb
Artem <proceller@yandex.ru>
2023-06-13 16:33:50 +0300

    end work

--- src/main/java/works/task1.java
+++ src/main/java/works/task1.java
@@ -1,2 +1,26 @@
-package works;public class task1 {
+package works;
+import java.util.HashMap;
+import java.util.Map;
+public class task1 {
+    public static void main(String[] args) {
+        Map<String, String> dict = new HashMap<String,String>();
+        dict.put("name","Ivanov");
+        dict.put("country","Russia");
+        dict.put("city","Moscow");
+        dict.put("age",null);
+        System.out.println(where(dict));
+    }
+    public static String where (Map<String, String> dict)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String,String> mapobj : dict.entrySet())
+        {
+            if (mapobj.getValue() != null)
+            {
+                sb.append(mapobj.getKey() +" = '" + mapobj.getValue()+"' and ");
+            }
+        }
+        sb.delete(sb.toString().length()-5,sb.toString().length());
+        return sb.toString();
+    }
 }
--- src/main/java/works/task2.java
+++ src/main/java/works/task2.java
@@ -1,2 +1,29 @@
-package works;public class task2 {
+package works;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class task2 {
+    private static final Logger LOGGER = Logger.getLogger(task2 .class.getName());
+
+    public static void main(String[] args) throws IOException {
+        FileHandler fh = new FileHandler("task2 .log");
+        LOGGER.addHandler(fh);
+        LOGGER.setLevel(Level.INFO);
+
+        int[] arr = new int[]{2, 5, 63, 23, 12, 2, 44, 66, 21, 7, 65};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            LOGGER.log(Level.INFO, "Iteration {0}: {1}", new Object[]{i+1, Arrays.toString(arr)});
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
+
